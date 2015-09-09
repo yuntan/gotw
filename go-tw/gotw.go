448e9315e34800059a96591cdd46f193661c7543
Yuto Tokunaga <yuntan.sub1@gmail.com>
2015-09-09 11:05:10 +0900

    [ADD] GetMentions, RemoveTweet

--- go-tw/gotw.go
+++ go-tw/gotw.go
@@ -1,12 +1,15 @@
 package tw
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 
 	"github.com/dghubble/oauth1"
 	"github.com/dghubble/oauth1/twitter"
 )
+
+const TWITTER_API = "https://api.twitter.com/1.1/"
 
 var conf *oauth1.Config
 
@@ -39,13 +42,27 @@ func GetAccessToken(reqToken *oauth1.RequestToken, pin string) (*oauth1.Token, e
 func VerifyCredentials(token *oauth1.Token) (*http.Response, error) {
 	conf.Endpoint = twitter.AuthenticateEndpoint
 	client := oauth1.NewClient(conf, token)
-	return client.Get("https://api.twitter.com/1.1/account/verify_credentials.json")
+	return client.Get(TWITTER_API + "account/verify_credentials.json")
 }
 
 // https://dev.twitter.com/rest/reference/post/statuses/update
 func Tweet(tweet string, token *oauth1.Token) (*http.Response, error) {
 	conf.Endpoint = twitter.AuthenticateEndpoint
 	client := oauth1.NewClient(conf, token)
-	return client.PostForm("https://api.twitter.com/1.1/statuses/update.json",
+	return client.PostForm(TWITTER_API+"statuses/update.json",
 		url.Values{"status": []string{tweet}})
+}
+
+// https://dev.twitter.com/rest/reference/get/statuses/mentions_timeline
+func GetMentions(token *oauth1.Token) (*http.Response, error) {
+	conf.Endpoint = twitter.AuthenticateEndpoint
+	client := oauth1.NewClient(conf, token)
+	return client.PostForm(TWITTER_API+"statuses/mentions_timeline.json", nil)
+}
+
+// https://dev.twitter.com/rest/reference/post/statuses/destroy/%3Aid
+func RemoveTweet(id string, token *oauth1.Token) (*http.Response, error) {
+	conf.Endpoint = twitter.AuthenticateEndpoint
+	client := oauth1.NewClient(conf, token)
+	return client.PostForm(TWITTER_API+fmt.Sprintf("statuses/destroy/%s.json", id), nil)
 }
