1ed1f492b05667bce65f6a756d7d5805a0fb9183
Yuto Tokunaga <yuntan.sub1@gmail.com>
2015-09-10 19:35:53 +0900

    remove consumerkey/secret from library

--- go-tw/gotw.go
+++ go-tw/gotw.go
@@ -11,23 +11,15 @@ import (
 
 const TWITTER_API = "https://api.twitter.com/1.1/"
 
-var conf *oauth1.Config
-
-func init() {
-	conf = &oauth1.Config{
-		ConsumerKey:    CONSUMER_KEY,
-		ConsumerSecret: CONSUMER_SECRET,
-		CallbackURL:    "oob",
-	}
-}
+var Config *oauth1.Config
 
 func GetRequestToken() (*oauth1.RequestToken, *url.URL, error) {
-	conf.Endpoint = twitter.AuthorizeEndpoint
-	reqToken, err := conf.GetRequestToken()
+	Config.Endpoint = twitter.AuthorizeEndpoint
+	reqToken, err := Config.GetRequestToken()
 	if err != nil {
 		return nil, nil, err
 	}
-	url, err := conf.AuthorizationURL(reqToken)
+	url, err := Config.AuthorizationURL(reqToken)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -35,34 +27,34 @@ func GetRequestToken() (*oauth1.RequestToken, *url.URL, error) {
 }
 
 func GetAccessToken(reqToken *oauth1.RequestToken, pin string) (*oauth1.Token, error) {
-	return conf.GetAccessToken(reqToken, pin)
+	return Config.GetAccessToken(reqToken, pin)
 }
 
 // https://dev.twitter.com/rest/reference/get/account/verify_credentials
 func VerifyCredentials(token *oauth1.Token) (*http.Response, error) {
-	conf.Endpoint = twitter.AuthenticateEndpoint
-	client := oauth1.NewClient(conf, token)
+	Config.Endpoint = twitter.AuthenticateEndpoint
+	client := oauth1.NewClient(Config, token)
 	return client.Get(TWITTER_API + "account/verify_credentials.json")
 }
 
 // https://dev.twitter.com/rest/reference/post/statuses/update
 func Tweet(tweet string, token *oauth1.Token) (*http.Response, error) {
-	conf.Endpoint = twitter.AuthenticateEndpoint
-	client := oauth1.NewClient(conf, token)
+	Config.Endpoint = twitter.AuthenticateEndpoint
+	client := oauth1.NewClient(Config, token)
 	return client.PostForm(TWITTER_API+"statuses/update.json",
 		url.Values{"status": []string{tweet}})
 }
 
 // https://dev.twitter.com/rest/reference/get/statuses/mentions_timeline
 func GetMentions(token *oauth1.Token) (*http.Response, error) {
-	conf.Endpoint = twitter.AuthenticateEndpoint
-	client := oauth1.NewClient(conf, token)
-	return client.PostForm(TWITTER_API+"statuses/mentions_timeline.json", nil)
+	Config.Endpoint = twitter.AuthenticateEndpoint
+	client := oauth1.NewClient(Config, token)
+	return client.Get(TWITTER_API + "statuses/mentions_timeline.json")
 }
 
 // https://dev.twitter.com/rest/reference/post/statuses/destroy/%3Aid
 func RemoveTweet(id string, token *oauth1.Token) (*http.Response, error) {
-	conf.Endpoint = twitter.AuthenticateEndpoint
-	client := oauth1.NewClient(conf, token)
+	Config.Endpoint = twitter.AuthenticateEndpoint
+	client := oauth1.NewClient(Config, token)
 	return client.PostForm(TWITTER_API+fmt.Sprintf("statuses/destroy/%s.json", id), nil)
 }
